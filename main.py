b735cfd883120a50bb27a9b10b18fd3da6690dd9
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-04-20 16:28:49 -0700

    added failure case fixes and moved main fn to main_loop

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from src.responder import QueryHandler
-from src.client.dataset_retriever.kaggle import KaggleDatasetClient
 
 if __name__ == "__main__":
-    # client = KaggleDatasetClient()
-
-    # print(client.list_datasets(""))
     atlas_example=[
         (
             'cmotions/NLrestaurantreviews',
