a618dca1d6e06db4c00b94e63d1ce64389396214
AbhigyaWangoo <abhigyawangoo@gmail.com>
2024-04-20 18:55:42 -0700

    Added dataset coalescence module

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-from src.process import main_loop
+from src.responder import QueryHandler
+from src.client.dataset_retriever.kaggle import KaggleDatasetClient
 
 if __name__ == "__main__":
-    main_loop(1)
+    # client = KaggleDatasetClient()
+
+    # print(client.list_datasets(""))
+    atlas_example=[
+        (
+            'cmotions/NLrestaurantreviews',
+            {
+                'freq': 9,
+                'name': 'cmotions/NLrestaurantreviews',
+                'links': 'https://huggingface.co/datasets/cmotions/NL_restaurant_reviews',
+                'dataset_summary': "The dataset contains restaurant reviews collected in 2019 from Dutch restaurants. It is formatted using the DatasetDict format and includes train, test, and validation indices with 116693, 14587, and 14587 records respectively. The dataset includes both restaurant and review level information. \n\nRestaurant level information includes a unique restaurant ID, a Michelin star indicator, and scores for total, food, service, and decor. Review level information includes a unique review ID, a label for the reviewer's frequency of posting, scores for food, service, ambiance, waiting, value for money, and noise, the full review text, and the total length of the review. \n\nThe dataset can be used to model restaurant scores or Michelin star holders. For example, the review texts were used in a blog series to predict the next Michelin star restaurants using R."
+            }
+        ),
+        (
+            'prasadsawant7/sentimentanalysispreprocesseddataset',
+            {
+                'freq': 1,
+                'name': 'prasadsawant7/sentimentanalysispreprocesseddataset',
+                'links': 'https://huggingface.co/datasets/prasadsawant7/sentiment_analysis_preprocessed_dataset',
+                'dataset_summary': "The dataset is designed for Text Classification, specifically Multi Class Classification, to train a model for Sentiment Analysis. It also allows for retraining the model based on feedback on incorrect sentiment predictions. The main features of the dataset are 'text' and 'labels'. 'Text' contains various forms of text like sentences, tweets, etc., while 'labels' contains numeric values representing sentiments - 0 for Negative, 1 for Neutral, and 2 for Positive. \n\nThe dataset also includes other features like 'preds', 'feedback', 'retrain_labels', and 'retrained_preds'. 'Preds' stores all predictions, 'feedback' allows users to indicate if a prediction is correct or not, 'retrain_labels' allows users to provide the correct label as feedback for retraining the model, and 'retrained_preds' stores all predictions after the feedback loop."
+            }
+        )
+    ]
+    query="restaurants with cuisine, price, and name"
+    print(QueryHandler().coalesce_response(atlas_example, query))
--- src/responder.py
+++ src/responder.py
@@ -1,5 +1,5 @@
 from src.client.gpt import GPTClient
-from typing import Dict, Any
+from typing import Dict, Any, Tuple, List
 
 class QueryHandler():
     """ 
@@ -10,19 +10,47 @@ class QueryHandler():
 
         self.coalescence_prompt="""
         You are a chatbot built to respond regarding user inquires about datasets. You must provide a response with only the information
-        provided. You will be given a dataset summary, a link to the dataset, and the user's initial query. Given all this information, 
-        respond to the user's initial query appropriately. You should aim to be a friendly chatbot that is able so seamlessly respond to
+        provided. You will be given the name of the dataset, a dataset summary, and the user's initial query. Keep in mind that the users' query
+        might be an example of an entry in a dataset, and might not be a natural language prompt. Given all this information, 
+        explain why the provided data might help the user. You should aim to be a friendly chatbot that is able so seamlessly respond to
         the user's question.
         """
 
-    def coalesce_response(self, atlas_res: Dict[str, Dict[str, Any]], initial_query: str) -> str:
-        """ 
-        Given the provided summary, link to dataset, and initial query, this 
-        function responds with a concise response for the user.
-        
-        atlas_res: a dict obeying the following schema: {dataset_id: {"summary": str, "links": str}}
+    def pretty_print(self, response: str, name: str, summary: str, link: str ) -> str:
+        """
+        return a Pretty print str out a response to the user
         """
 
-        response = self.llm_client.query(initial_query, sys_prompt=self.coalescence_prompt)
+        response_format="%s\n\n%s\n\nDataset summary: %s\nlink to dataset: %s\n<------------>\n<------------>"
+
+        return response_format % (name, response, summary, link)
+
+    def coalesce_response(self, atlas_res: List[Tuple[str, Dict[str, Any]]], initial_query: str) -> str: # Ideally shouldve wrapped the response in a class. oops.
+        """
+        Provided with a response from mongodb atlas, response to the user appropriately
+        """
+        responses=[]
+        for dataset in atlas_res:
+            name=dataset[0]
+            metadata=dataset[1]
+
+            link=metadata['links'] if 'links' in metadata else None
+            summary=metadata['dataset_summary'].replace('\n',' ') if 'dataset_summary' in metadata else None
+
+            response = self.coalesce_a_response(initial_query, summary)
+
+            responses.append(self.pretty_print(response, name, summary, link))
+
+        return '\n\n'.join(responses)
+
+    def coalesce_a_response(self, initial_query: str, summary: str) -> str:
+        """ 
+        Given the provided summary, and initial query, this 
+        function responds with a concise response for the user.
+        """
+
+        final_prompt = f"initial query: {initial_query}\nsummary of found dataset: {summary}"
+
+        response = self.llm_client.query(final_prompt, sys_prompt=self.coalescence_prompt)
 
         return response
